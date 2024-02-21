19f9182907808a8231fe53e5eb64de015a519dd8
AlonaSkobukh <alenaskobukh@gmail.com>
2024-02-22 01:36:38 +0200

    hw

---
+++ hw/main.js
@@ -0,0 +1,59 @@
+let a ='hello';
+console.log(a);
+let b ='owu';
+console.log(b);
+let c ='com';
+console.log(c);
+let d ='ua';
+console.log(d);
+
+let n1=1;
+console.log(n1);
+let n2=10;
+console.log(n2);
+let n3=-999;
+console.log(n3);
+let n4=123;
+console.log(n4);
+let n5=3.14;
+console.log(n5);
+let n6=2.7;
+console.log(n6);
+let n7=16;
+console.log(n7);
+
+let bool1=true;
+console.log(bool1);
+let bool2=false;
+console.log(bool2);
+
+let firstName='Alona';
+console.log(firstName);
+let middleName='Skobukh';
+console.log(middleName);
+let lastName='Sergiivna';
+console.log(lastName);
+let person={
+    firstName: 'Alona',
+    middleName:'Skobukh',
+    lastName:'Sergiivna'
+}
+console.log(person);
+
+
+let i=100;
+console.log(typeof i);
+let f='100';
+console.log(typeof f);
+let g=true;
+console.log(typeof g);
+
+
+
+
+
+
+
+
+
+
