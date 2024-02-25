8c3ae006dcd65eb6ed3458366d54c274f55f184d
AlonaSkobukh <alenaskobukh@gmail.com>
2024-02-25 19:56:33 +0200

    hw1

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -160,32 +160,38 @@ switch (date) {
         break;
 }
 
-
   //  - Користувач вводить або має два числа.
 let a=8;
 let b=3;
     //    Потрібно знайти та вивести максимальне число з тих двох .
 if (a>b) {console.log(`"a"більше за "b"`);}
-//else if (a<b) {console.log(`"a"менше за "b"`);}
+else if (a<b) {console.log(`"a"менше за "b"`);}
       //  Також потрібно врахувати коли введені рівні числа.
 else {console.log(`"a" дорівнює "b"`);}
 
     //- є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
       //  за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
-
-//let x=12
-
-
+let c= ``||`default`
+console.log(c)
     //- з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
 
-//let coursesAndDurationArray = [
-    //{title: 'JavaScript Complex', monthDuration: 5},
-   // {title: 'Java Complex', monthDuration: 6},
-    //{title: 'Python Complex', monthDuration: 6},
-    //{title: 'QA Complex', monthDuration: 4},
-   // {title: 'FullStack', monthDuration: 7},
-   // {title: 'Frontend', monthDuration: 4}
-//];
-//if (x>5) {
-  //  console.log(`Супер`)
-//}
+if (coursesAndDurationArray[0].monthDuration>=5) {console.log(`Супер`);}
+else {console.log(`Bed`)}
+if (coursesAndDurationArray[1].monthDuration>=5) {console.log(`Супер`);}
+else {console.log(`Bed`)}
+if (coursesAndDurationArray[2].monthDuration>=5) {console.log(`Супер`);}
+else {console.log(`Bed`)}
+if (coursesAndDurationArray[3].monthDuration>=5) {console.log(`Супер`);}
+else {console.log(`Bed`)}
+if (coursesAndDurationArray[4].monthDuration>=5) {console.log(`Супер`);}
+else {console.log(`Bed`)}
+if (coursesAndDurationArray[5].monthDuration>=5) {console.log(`Супер`);}
+else {console.log(`Bed`)}
