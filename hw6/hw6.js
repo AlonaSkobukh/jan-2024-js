8eb481db77d01c4657da91d88ca54711d0ef2da6
AlonaSkobukh <alenaskobukh@gmail.com>
2024-03-06 02:08:49 +0200

    hw

--- hw6/hw6.js
+++ hw6/hw6.js
@@ -43,21 +43,55 @@ let str6=' dirty string   ';
 console.log(str6.trim());
 
 
-//- Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+//- Напишіть функцію stringToArray(str), яка перетворює рядок на масив слів.
 //    let str = 'Ревуть воли як ясла повні';
-//let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+//let arr = stringToArray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
 
 let str7 = 'Ревуть воли як ясла повні';
 let arr=str7.split(` `);
 console.log(arr)
 
-//
+
+
 //- є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
 let numbers=[10,8,-7,55,987,-1011,0,1050,0];
+//let map=numbers.map((number)=>{
+//    let m= {
+//        number:``
+//    }
+//    return m
+//});
+//console.log(map)
+//-----------------------------------------------------------------
+let arr1=numbers.toString();
+console.log(arr1)
+//-----------------------------------------------------------------
+//let str8=String(numbers);
+//let arr2=str8.split(``);
+//console.log(arr2)
+
 
 
 //- створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
     let nums = [11,21,3];
+
+    let sort=(arr,direction) => {
+
+        if (direction === 'ascending') {
+            arr.sort((a, b) => {
+                return a - b
+
+            });
+        } else if (direction === 'descending') {
+            arr.sort((a, b) => {
+                return b - a
+            });
+        }
+        return arr;
+    }
+        console.log(sort(nums,`descending`)  )
+
+
 //sortNums(nums,'ascending') // [3,11,21]
 //sortNums(nums,'descending') // [21,11,3]
 //
@@ -72,11 +106,41 @@ let coursesAndDurationArray = [
    {title: 'Frontend', monthDuration: 4}
     ]
 ;
-//-- відсортувати його за спаданням за monthDuration
 //-- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+let filter=coursesAndDurationArray.filter((
+    coursesAndDurationArrayKey)=>coursesAndDurationArrayKey.monthDuration>5);
+console.log(filter);
+
+
+
+//-- відсортувати його за спаданням за monthDuration
+//let filter=coursesAndDurationArray.filter((
+    //coursesAndDurationArrayKey)=>coursesAndDurationArrayKey.monthDuration=== );
+//console.log(filter);
+
+
 //-- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+
+//let map=coursesAndDurationArray.map((coursesAndDurationArrayKey,index)=>{
+//    let m= {
+//        title:coursesAndDurationArrayKey.title,
+//        monthDuration:coursesAndDurationArrayKey.monthDuration,
+//        id:index
+//}
+//return m;
+//});
+//console.log(map)
+//------------------------------------------------------
+    let map=coursesAndDurationArray.map((coursesAndDurationArrayKey,index)=>{
+        coursesAndDurationArrayKey.id=index+1;
+        return coursesAndDurationArrayKey
+    });
+console.log(map)
+console.log(coursesAndDurationArray)
 //=========================
 //    описати колоду карт (від 6 до туза без джокерів)
+
+
 //- знайти піковий туз
 //- всі шістки
 //- всі червоні карти
@@ -99,9 +163,11 @@ let coursesAndDurationArray = [
 //    clubs:[]
 //}
 //=========================
+
+
+
 //    взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
-//--написати пошук всіх об'єктів, в який в modules є sass
-//--написати пошук всіх об'єктів, в який в modules є docker
+
 let coursesArray = [
     {
         title: 'JavaScript Complex',
@@ -171,3 +237,33 @@ let coursesArray = [
         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
     }
 ];
+
+//--написати пошук всіх об'єктів, в який в modules є sass
+let modulesSass = (arr) => {
+    return arr.find((item)=> item.modules.includes(`sass` )
+
+    );
+
+}
+console.log(modulesSass(coursesArray));
+
+
+//--написати пошук всіх об'єктів, в який в modules є docker
+
+const modulesDocker = (arr) => {
+    return arr.filter((item)=> item.modules.includes(`docker` )
+
+    );
+
+}
+console.log(modulesDocker(coursesArray));
+
+//const modulesDocker = (arr) => {
+//    return arr.filter((item)=> item.modules.some(elem => elem===`docker` )
+//
+//    );
+//
+//}
+//console.log(modulesDocker(coursesArray));
+
+
