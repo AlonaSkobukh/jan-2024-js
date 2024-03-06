c41428e9ce9accab4bd0caa6fa40b6ec01d5fd0d
AlonaSkobukh <alenaskobukh@gmail.com>
2024-03-06 22:11:12 +0200

    hw

--- hw6/hw6.js
+++ hw6/hw6.js
@@ -55,21 +55,9 @@ console.log(arr)
 
 //- є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
 let numbers=[10,8,-7,55,987,-1011,0,1050,0];
-//let map=numbers.map((number)=>{
-//    let m= {
-//        number:``
-//    }
-//    return m
-//});
-//console.log(map)
-//-----------------------------------------------------------------
+
 let arr1=numbers.toString();
 console.log(arr1)
-//-----------------------------------------------------------------
-//let str8=String(numbers);
-//let arr2=str8.split(``);
-//console.log(arr2)
-
 
 
 //- створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
@@ -112,89 +100,90 @@ let filter=coursesAndDurationArray.filter((
 console.log(filter);
 
 
-
 //-- відсортувати його за спаданням за monthDuration
-//let filter=coursesAndDurationArray.filter((
-    //coursesAndDurationArrayKey)=>coursesAndDurationArrayKey.monthDuration=== );
-//console.log(filter);
+coursesAndDurationArray.sort(
+    (a,b
+    )=>b.monthDuration-a.monthDuration);
 
+console.log(coursesAndDurationArray)
 
 //-- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
 
-//let map=coursesAndDurationArray.map((coursesAndDurationArrayKey,index)=>{
-//    let m= {
-//        title:coursesAndDurationArrayKey.title,
-//        monthDuration:coursesAndDurationArrayKey.monthDuration,
-//        id:index
-//}
-//return m;
-//});
-//console.log(map)
-//------------------------------------------------------
-    let map=coursesAndDurationArray.map((coursesAndDurationArrayKey,index)=>{
-        coursesAndDurationArrayKey.id=index+1;
-        return coursesAndDurationArrayKey
-    });
+let map=coursesAndDurationArray.map((coursesAndDurationArrayKey,index)=>{
+    let m= {
+        title:coursesAndDurationArrayKey.title,
+        monthDuration:coursesAndDurationArrayKey.monthDuration,
+        id:index
+}
+return m;
+});
 console.log(map)
-console.log(coursesAndDurationArray)
+//------------------------------------------------------
+//   let map=coursesAndDurationArray.map((coursesAndDurationArrayKey,index)=>{
+//       coursesAndDurationArrayKey.id=index+1;
+//       return coursesAndDurationArrayKey
+//   });
+//console.log(map)
+//console.log(coursesAndDurationArray)
 
 
 //=========================
 
 
-
-
 //    описати колоду карт (від 6 до туза без джокерів)
 
-const cardSuits=[
+let cards=[
 
-{cardSuit: 'spades', value: 6, color: 'black'};
-{cardSuit: 'spades', value: 7, color: 'black'};
-{cardSuit: 'spades', value: 8, color: 'black'};
-{cardSuit: 'spades', value: 9, color: 'black'};
-{cardSuit: 'spades', value: 10, color: 'black'};
-{cardSuit: 'spades', value: 'ace', color: 'black'};
-{cardSuit: 'spades', value: 'jack', color: 'black'};
-{cardSuit: 'spades', value: 'queen', color: 'black'};
-{cardSuit: 'spades', value: 'king', color: 'black'};
-{cardSuit: 'spades', value: 'joker', color: 'black'};
-{cardSuit: 'diamonds', value: 6, color: 'red'};
-{cardSuit: 'diamonds', value: 7, color: 'red'};
-{cardSuit: 'diamonds', value: 8, color: 'red'};
-{cardSuit: 'diamonds', value: 9, color: 'red'};
-{cardSuit: 'diamonds', value: 10, color: 'red'};
-{cardSuit: 'diamonds', value: 'ace', color: 'red'};
-{cardSuit: 'diamonds', value: 'jack', color: 'red'};
-{cardSuit: 'diamonds', value: 'queen', color: 'red'};
-{cardSuit: 'diamonds', value: 'king', color: 'red'};
-{cardSuit: 'diamonds', value: 'joker', color: 'red'};
-{cardSuit: 'hearts', value: 6, color: 'red'};
-{cardSuit: 'hearts', value: 7, color: 'red'};
-{cardSuit: 'hearts', value: 8, color: 'red'};
-{cardSuit: 'hearts', value: 9, color: 'red'};
-{cardSuit: 'hearts', value: 10, color: 'red'};
-{cardSuit: 'hearts', value: 'ace', color: 'red'};
-{cardSuit: 'hearts', value: 'jack', color: 'red'};
-{cardSuit: 'hearts', value: 'queen', color: 'red'};
-{cardSuit: 'hearts', value: 'king', color: 'red'};
-{cardSuit: 'hearts', value: 'joker', color: 'red'};
-{cardSuit: 'clubs', value: 6, color: 'black'};
-{cardSuit: 'clubs', value: 7, color: 'black'};
-{cardSuit: 'clubs', value: 8, color: 'black'};
-{cardSuit: 'clubs', value: 9, color: 'black'};
-{cardSuit: 'clubs', value: 10, color: 'black'};
-{cardSuit: 'clubs', value: 'ace', color: 'black'};
-{cardSuit: 'clubs', value: 'jack', color: 'black'};
-{cardSuit: 'clubs', value: 'queen', color: 'black'};
-{cardSuit: 'clubs', value: 'king', color: 'black'};
-{cardSuit: 'clubs', value: 'joker', color: 'black'};]
+{cardSuit: 'spades', value: 6, color: 'black'},
+{cardSuit: 'spades', value: 7, color: 'black'},
+{cardSuit: 'spades', value: 8, color: 'black'},
+{cardSuit: 'spades', value: 9, color: 'black'},
+{cardSuit: 'spades', value: 10, color: 'black'},
+{cardSuit: 'spades', value: 'ace', color: 'black'},
+{cardSuit: 'spades', value: 'jack', color: 'black'},
+{cardSuit: 'spades', value: 'queen', color: 'black'},
+{cardSuit: 'spades', value: 'king', color: 'black'},
 
 
 
+{cardSuit: 'diamonds', value: 6, color: 'red'},
+{cardSuit: 'diamonds', value: 7, color: 'red'},
+{cardSuit: 'diamonds', value: 8, color: 'red'},
+{cardSuit: 'diamonds', value: 9, color: 'red'},
+{cardSuit: 'diamonds', value: 10, color: 'red'},
+{cardSuit: 'diamonds', value: 'ace', color: 'red'},
+{cardSuit: 'diamonds', value: 'jack', color: 'red'},
+{cardSuit: 'diamonds', value: 'queen', color: 'red'},
+{cardSuit: 'diamonds', value: 'king', color: 'red'},
 
+
+
+{cardSuit: 'hearts', value: 6, color: 'red'},
+{cardSuit: 'hearts', value: 7, color: 'red'},
+{cardSuit: 'hearts', value: 8, color: 'red'},
+{cardSuit: 'hearts', value: 9, color: 'red'},
+{cardSuit: 'hearts', value: 10, color: 'red'},
+{cardSuit: 'hearts', value: 'ace', color: 'red'},
+{cardSuit: 'hearts', value: 'jack', color: 'red'},
+{cardSuit: 'hearts', value: 'queen', color: 'red'},
+{cardSuit: 'hearts', value: 'king', color: 'red'},
+
+
+{cardSuit: 'clubs', value: 6, color: 'black'},
+{cardSuit: 'clubs', value: 7, color: 'black'},
+{cardSuit: 'clubs', value: 8, color: 'black'},
+{cardSuit: 'clubs', value: 9, color: 'black'},
+{cardSuit: 'clubs', value: 10, color: 'black'},
+{cardSuit: 'clubs', value: 'ace', color: 'black'},
+{cardSuit: 'clubs', value: 'jack', color: 'black'},
+{cardSuit: 'clubs', value: 'queen', color: 'black'},
+{cardSuit: 'clubs', value: 'king', color: 'black'},
+    ]
+
+//---------------------------------------------------------
 
 let cardSuits=['spades', 'diamonds','hearts', 'clubs'];
-let values=[6,7,8,9,10, 'ace','jack','queen','king','joker'];
+let values=[6,7,8,9,10, 'ace','jack','queen','king'];
 
 
 let desk=() =>{
@@ -204,7 +193,7 @@ let desk=() =>{
            desks.push({  cardSuit: cardSuit,value:value  })
 
         }
-        
+
     }
     desks.map(item=>item.cardSuit === 'diamonds'||item.cardSuit === 'hearts' ? item.color = `red` :item.color = `black`)
 return desks
@@ -213,10 +202,30 @@ console.log(desk());
 
 
 //- знайти піковий туз
+let ace=cards.filter(card=>card.value===`ace`&& card.cardSuit==='spades');
+console.log(ace)
+
+
 //- всі шістки
+
+let six=cards.filter(card=>card.value===6);
+console.log(six)
+
+
 //- всі червоні карти
+
+let red=cards.filter(card=>card.color==='red');
+console.log(red)
+
 //- всі буби
+let Diamond=cards.filter(card=>card.cardSuit==='diamonds');
+console.log(Diamond)
+
 //- всі трефи від 9 та більше
+let club=cards.filter(card=>card.cardSuit==='clubs').splice(3);
+console.log(club)
+
+
 //
 //{
 //    cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
@@ -233,9 +242,27 @@ console.log(desk());
 //    hearts:[],
 //    clubs:[]
 //}
+let reduce=cards.reduce(
+    (accumulator, card) => {
+        if (card.cardSuit===`spades`) {
+            accumulator[0].push(card);
+        }
+        else if (card.cardSuit===`diamonds`) {
+            accumulator[1].push(card);
+        }
+        else if (card.cardSuit===`hearts`) {
+            accumulator[2].push(card);
+        }
+        else if (card.cardSuit===`clubs`) {
+            accumulator[3].push(card);
+        }
+        return accumulator;
+    },
+    [[],[],[],[]]
+);
+console.log(reduce);
+
 //=========================
-
-
 
 //    взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
 
